5ebc1cf9f511621a1bd858171f08f67719f62728
Roc <cuipengxiang@gmail.com>
2013-11-25 22:32:23 +0800

    模式选择页面

---
+++ SmartHouse/SHModeSelectViewController.h
@@ -0,0 +1,27 @@
+//
+//  SHModeSelectViewController.h
+//  SmartHouse
+//
+//  Created by Roc on 13-11-25.
+//  Copyright (c) 2013年 Roc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SHStateViewController.h"
+#import "AppDelegate.h"
+#import "SHRoomModel.h"
+
+@interface SHModeSelectViewController : SHStateViewController
+
+@property(nonatomic, strong)AppDelegate *myAppDelegate;
+@property (nonatomic, strong)SHRoomModel *model;
+@property(nonatomic, strong)UINavigationBar *navigationBar;
+@property(nonatomic, strong)UIButton *networkStateButton;
+@property(nonatomic, strong)UIBarButtonItem *networkBarButton;
+@property(nonatomic, strong)UINavigationItem *item;
+
+- (void)setupNavigationBar:(float)width;
+- (void)onBackButtonClick;
+- (void)onSettingButtonClick;
+
+@end
---
+++ SmartHouse/SHModeSelectViewController.m
@@ -0,0 +1,38 @@
+//
+//  SHModeSelectViewController.m
+//  SmartHouse
+//
+//  Created by Roc on 13-11-25.
+//  Copyright (c) 2013年 Roc. All rights reserved.
+//
+
+#import "SHModeSelectViewController.h"
+
+@interface SHModeSelectViewController ()
+
+@end
+
+@implementation SHModeSelectViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
